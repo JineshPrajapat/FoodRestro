5071391ff3ecf58091c3970a71dc2e052615cd05
Jinesh Prajapat <prajapatjinesh585@gmail.com>
2023-02-24 17:47:17 +0530

    restro

---
+++ style.js
@@ -0,0 +1,9 @@
+const socialMediaLinks = document.querySelectorAll('.social-media-links a');
+
+socialMediaLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const url = link.getAttribute('href');
+    window.open(url, '_blank');
+  });
+});
